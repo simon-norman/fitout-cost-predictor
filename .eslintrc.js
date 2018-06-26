2a7ae11a82a8305bf95651ee89f4a86189937b67
Simon Norman <01secn@gmail.com>
2018-06-26 17:53:12 +0100

    Initial commit of Vue webpack

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    parser: 'babel-eslint',
+    ecmaVersion: 2017
+  },
+  env: {
+    browser: true,
+    "jest": true
+  },
+ 
+  "extends": [
+    "airbnb-base",
+    "plugin:vue/recommended"
+  ],
+  // required to lint *.vue files
+  "plugins": [
+    'vue'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-trailing-spaces': 0, // set it to 1 to get a warning.,
+    'linebreak-style': ["error", "unix"],
+    "import/no-unresolved": "off",
+    "no-restricted-syntax": ["error", "ForInStatement", "LabeledStatement", "WithStatement"],
+    "no-underscore-dangle": 0,
+    "import/prefer-default-export": 0,
+    "no-param-reassign": 0,
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions,
+} from 'vuetify';
+
+import App from './App.vue';
+import router from './router';
+
+import '../node_modules/vuetify/src/stylus/app.styl';
+
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions,
+  },
+});
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+});
