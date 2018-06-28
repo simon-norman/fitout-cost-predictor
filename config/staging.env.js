ef3bf0cf3595916f02f77e1ff57ccc3924ef2e51
Simon Norman <01secn@gmail.com>
2018-06-28 08:57:38 +0100

    Added staging enivornment build

---
+++ config/staging.env.js
@@ -0,0 +1,10 @@
+// create this file at config/staging.env.js
+// the name and NODE_ENV (below) must match the env-name you added in build/webpack.prod.conf.js
+
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"staging"',
+})
