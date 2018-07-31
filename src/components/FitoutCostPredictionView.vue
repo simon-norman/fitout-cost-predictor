56dc22648e212015ddd0fa37bd04f665c4fc038e
Simon Norman <01secn@gmail.com>
2018-07-31 12:11:48 +0100

    Added cost prediction view page to separate out the cost prediction report into separate page from the prediction input form

---
+++ src/components/FitoutCostPredictionView.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-content>
+    <v-container 
+      fluid 
+      fill-height>
+      <v-layout 
+        justify-center
+        align-center>
+        <v-flex 
+          xs12 
+          sm7 
+          md5
+          lg4
+          xl3>
+          <v-layout 
+            v-for="(fitoutCostItem, index) in fitoutCostItems"
+            :class="getCostItemClass(fitoutCostItem.costCategory)"
+            :key="index"
+            row>
+            <v-layout column>
+              <div 
+                id="displayedCostPrediction">{{ fitoutCostItem.costCategory }}:</div>
+            </v-layout> 
+            <v-layout 
+              column 
+              align-end>
+              <div 
+                id="displayedCostPrediction">{{ fitoutCostItem.cost }} </div>
+            </v-layout>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-content>
+</template>
+
+<script>
+
+export default {
+  name: 'FitoutCostPredictionView',
+
+  data() {
+    return {
+      fitoutCostItems: [
+        { costCategory: 'Total', cost: '£5m' },
+        { costCategory: 'Construction', cost: '£2m' },
+        { costCategory: 'M&E', cost: '£1.5m' },
+        { costCategory: 'Furniture', cost: '£250k' },
+        { costCategory: 'AV / IT', cost: '£750k' },
+        { costCategory: 'Fees', cost: '£500k' },
+      ],
+    };    
+  },
+
+  methods: {
+    getCostItemClass(costCategory) {
+      if (costCategory === 'Total') {
+        return { 'cost-total': true };
+      } 
+      return { 'cost-item': true };
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import FitoutCostPredictor from '@/components/FitoutCostPredictor.vue';
+import FitoutCostPredictionView from '@/components/FitoutCostPredictionView.vue';
 import Alert from '@/components/Alert.vue';
 import Header from '@/components/Header.vue';
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'FitoutCostPredictor',
       components: { default: FitoutCostPredictor, alert: Alert, header: Header },
+    },
+    {
+      path: '/costPrediction/view',
+      name: 'FitoutCostPredictor',
+      components: { default: FitoutCostPredictionView, alert: Alert, header: Header },
     },
   ],
 });
